2aa30ce6bbc95a0a4936576dfc506b3ea432042a
dericsp <dericsp@gmail.com>
2019-12-05 11:04:44 -0300

    client folder

---
+++ client/build/precache-manifest.2461f90f25910110fae2affce7fe4c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c766e07a6d56fa2f644aeb55d3bad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae111b1af17b5a83f014",
+    "url": "/static/css/main.4492ded8.chunk.css"
+  },
+  {
+    "revision": "a2ca0323c59caec3fd5b",
+    "url": "/static/js/2.2f54e23e.chunk.js"
+  },
+  {
+    "revision": "ae111b1af17b5a83f014",
+    "url": "/static/js/main.d2ba72d4.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
